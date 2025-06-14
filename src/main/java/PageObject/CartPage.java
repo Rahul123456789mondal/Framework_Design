78d6f9309ce29c3f466f5b16ecd427692a32f586
CODECLOUDS-ARKA <arka.mondal@codeclouds.co.in>
2025-06-15 01:59:18 +0530

    Experimenter Folder Structure New Update - 15-06-2025

---
+++ src/main/java/PageObject/CartPage.java
@@ -0,0 +1,61 @@
+package PageObject;
+
+import AbstractComponents.AbstractComponent;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CartPage extends AbstractComponent {
+
+    WebDriver driver;
+
+    public CartPage (WebDriver driver){
+        super(driver);
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    // xPath Annotation
+    @FindBy(css = "[routerlink*='cart']")
+    private WebElement cartButton;
+
+    @FindBy(xpath = "//button[contains(text(),'Checkout')]")
+    private WebElement checkoutButton;
+
+    @FindBy(xpath = "//div[@class='cartSection']//h3")
+    private WebElement verifyProductName;
+
+
+
+    // Go to cart page
+    public void goToCart() {
+        try {
+            // Debug: Check if element is found
+            if (cartButton == null) {
+                System.out.println("CartButton is null - PageFactory not initialized");
+            }else {
+                cartButton.click();
+            }
+
+        } catch (Exception e) {
+            System.out.println("Failed to navigate to cart: " + e.getMessage());
+        }
+    }
+
+    // Proceed to checkout
+    public void proceedToCheckout() {
+        try {
+            waitForElementToBeClickable(checkoutButton);
+            checkoutButton.click();
+            //((JavascriptExecutor) driver).executeScript("button click()", checkoutButton);
+        } catch (Exception e) {
+            System.out.println("Failed to click checkout: " + e.getMessage());
+        }
+    }
+
+    public boolean verifyProductName(String productName){
+        return verifyProductName.getText().equalsIgnoreCase(productName);
+    }
+
+}
