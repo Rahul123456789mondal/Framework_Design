410ccb43675008ea854263eefdfea572f72f2624
CODECLOUDS-ARKA <arka.mondal@codeclouds.co.in>
2025-06-24 20:06:18 +0530

    New Update  - 24-06-2025

--- src/main/java/PageObject/CheckOutPage.java
+++ src/main/java/PageObject/CheckOutPage.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import javax.swing.*;
 import java.util.List;
 
 public class CheckOutPage {
